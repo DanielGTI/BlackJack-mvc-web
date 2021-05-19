12c8506e1cccdb41818398c4d356d56e25bab512
Daniel <daniel.fb.jr@gmail.com>
2021-05-19 20:00:26 -0300

    Jogo versão 0.1

---
+++ ExemploMVC/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function calcularIMC() {
+    var formulario = document.getElementById("formulario");
+    var peso = +formulario.peso.value;
+    var altura = +formulario.altura.value;
+    var imc = peso / (altura * altura);
+    formulario.imc.value = imc.toFixed(2);
+}
