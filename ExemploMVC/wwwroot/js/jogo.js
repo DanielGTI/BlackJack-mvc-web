7727b0694d75b6d01ae4a685ce1191f69dc1f7bc
Daniel <daniel.fb.jr@gmail.com>
2021-05-19 21:44:23 -0300

    Version 0.4

---
+++ ExemploMVC/wwwroot/js/jogo.js
@@ -0,0 +1,32 @@
+﻿
+function valorAleatorio() {
+    min = Math.ceil(1);
+    max = Math.floor(13);
+    var aleatorio = Math.floor(Math.random() * (max - min)) + min;
+    return aleatorio;
+}
+
+function jogador1() {
+
+    var cartas1 = document.getElementById("cartas1");
+    $(cartas1).attr("src", '/images/' + valorAleatorio() + '.png'); 
+
+}
+
+
+
+function parar1() {
+
+    var btn_jogador1 = document.getElementById("btn_jogador_1");
+    var btn_jogador2 = document.getElementById("btn_jogador_2");
+    var btn_parar1 = document.getElementById("btn_parar_1");
+    var btn_parar2 = document.getElementById("btn_parar_2");
+
+    $(btn_jogador1).attr('disabled', 'disabled');
+    $(btn_parar1).attr('disabled', 'disabled');
+
+    $(btn_jogador2).removeAttr('disabled');
+    $(btn_parar2).removeAttr('disabled');
+
+
+}
